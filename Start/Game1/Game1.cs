59407fc2393645ff4e0b259efa3df0493c5c724c
yamadayuji <yu46883@gmail.com>
2019-09-27 16:20:56 +0900

    ブランチどうしで競合させます

--- Start/Game1/Game1.cs
+++ Start/Game1/Game1.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 /// <summary>
-/// プロジェクト名がnamespaceとなります trialブランチに変更を加えた
+/// プロジェクト名がnamespaceとなります trialブランチに変更を加えた　これからこいつを競合させます
 /// </summary>
 namespace Game1 //コミットテスト
 {
